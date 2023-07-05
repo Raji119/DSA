60278900fa1544e1c2b7560865431171caab2406
raji119 <sudhakarraji46@gmail.com>
2023-07-05 19:19:56 +0530

    Getting Element By Index

---
+++ Arrays/Get.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Array{
+ int *A;
+ int size;
+ int length;
+};
+
+int get(struct Array *arr,int index) {
+ if(index>=0 && index<arr->length)
+    return arr->A[index];
+ return -1;
+}
+
+void display(struct Array arr){
+ int i;
+ printf("\nThe Elements are:\n");
+ for(i=0;i<arr.length;i++)
+  printf("%d ",arr.A[i]);
+}
+
+
+int main(){
+ struct Array arr;
+ int index,i;
+ printf("Enter Size of an Array:");
+ scanf("%d",&arr.size);
+
+ arr.A=(int *)malloc(arr.size*sizeof(int));
+
+ printf("Enter Array Limit:");
+ scanf("%d",&arr.length);
+ printf("Enter All Elements:");
+ for(i=0;i<arr.length;i++)
+  scanf("%d",&arr.A[i]);
+ 
+ display(arr);
+ printf("Enter Index:");
+ scanf("%d",&index);
+
+ printf("\nElement:%d",get(&arr,index));
+ display(arr);
+
+ return 0;
+}
